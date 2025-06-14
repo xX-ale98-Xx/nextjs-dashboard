5ac65931fab8d67c1d5b50aa77aa95b66186b205
Alessandro <nica.ale98@gmail.com>
2025-06-14 19:32:06 +0200

    add error handling for create, modify and delete invoices. Complete step 13 of the next.js tutorial.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
+  try{
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  }
+  catch (error) {
+    console.error('Error creating invoice:', error);
+    throw new Error('Failed to create invoice');
+  } 
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -46,18 +52,23 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
+  try{
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  }
+  catch (error) {
+    console.error('Error updating invoice:', error);
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
