176ae10234339810b85df57a3f43ebb2345a6ce0
Alessandro <nica.ale98@gmail.com>
2025-06-17 08:46:18 +0200

    Add next-auth to the project. Almost complete, stopped at chapter 15 halfway through.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,11 +1,10 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+
  
 export default async function Page() {
 
