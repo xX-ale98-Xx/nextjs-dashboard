92cdb1e94e2619f1fdf8a335b000af90b7c1023c
Alessandro <nica.ale98@gmail.com>
2025-06-27 18:09:06 +0200

    Add some metadata for sharing the website on social media

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,42 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | OMP! Dashboard',
+    default: 'OMP! Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
+  description: 'OMP è la nuova piattaforma online che aiuta i professionisti della riabilitazione muscolo-scheletrica.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+openGraph: {
+    title: 'OMP! Dashboard',
+    description: 'OMP è la nuova piattaforma online che aiuta i professionisti della riabilitazione muscolo-scheletrica.',
+    url: 'https://next-learn-dashboard.vercel.sh',
+    siteName: 'OMP! - OhMyPhysio',
+    images: [
+      {
+        url: 'https://nextjs-dashboard-ecru-eight-84.vercel.app/OMP_logo_rsz-removebg.png',
+        width: 1200,
+        height: 630,
+        alt: 'OMP! - OhMyPhysio',
+      },
+    ],
+    locale: 'it_IT',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'OMP! - OhMyPhysio',
+    description: 'OMP è la nuova piattaforma online che aiuta i professionisti della riabilitazione muscolo-scheletrica.',
+    site: '@OMPDashboard',
+    images: ['https://nextjs-dashboard-ecru-eight-84.vercel.app/OMP_logo_rsz-removebg.png'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+  alternates: {
+    canonical: 'https://next-learn-dashboard.vercel.sh',
+  },
 };
 
 export default function RootLayout({
--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
-// import Link from 'next/link';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <div className="flex flex-row h-20 w-full border-b border-gray-border overflow-hidden items-center justify-between pl-3 pr-6 md:pl-4 md:pr-8">
-      <div className="relative h-10 aspect-[631/211] hidden md:block">
-        <Image
-          src="/OMP_logo_rsz-removebg.png"
-          alt="Logo"
-          fill
-          className="object-contain"
-        />
-      </div>
+      <Link href="/">
+        <div className="relative h-10 aspect-[631/211] hidden md:block">
+          <Image
+            src="/OMP_logo_rsz-removebg.png"
+            alt="Logo"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </Link>
       <div className="relative h-12 aspect-[631/211] hidden md:block">
         <Image
           src="/OMP_logo_rsz-removebg.png"
