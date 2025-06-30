c915192c4c9e4bf9c882ead97044a4df8da834d5
Alessandro <nica.ale98@gmail.com>
2025-06-30 14:30:55 +0200

    Minor fix to hover effect in dashboard nav links Text color imposed to "brand-main" and font-semibold when hovered

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -33,7 +33,7 @@ export default function NavLinks() {
             className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-brand-main text-white': pathname === link.href,
-                'hover:bg-brand-background hover:text-black': pathname !== link.href,
+                'hover:bg-brand-background hover:text-brand-main hover:font-semibold': pathname !== link.href,
               }
             )}
           >
